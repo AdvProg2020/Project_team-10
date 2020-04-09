566648ec08367b8aa741e734c1ab319a37eace5a
MohammadJavad Alaeddini <javad1379.a@gmail.com>
2020-04-09 18:35:28 +0430

    Add Fields Of Log and Off

--- src/main/java/model/Log.java
+++ src/main/java/model/Log.java
@@ -1,4 +1,16 @@
 package model;
 
+import java.util.Date;
+import java.util.List;
+
 public class Log {
+    private String logId;
+    private Date date;
+    private long paidAmount;
+    private long discount;
+    private List<Good> goods;
+    private String name;
+    private String logStatus;
+
+
 }
---
+++ src/main/java/model/Off.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Off {
+    private String offId;
+    private List<Good> offGoods;
+    private String offStatus;
+    private Date startDate;
+    private Date endDate;
+    private int discount;
+
+
+}
