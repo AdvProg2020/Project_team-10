db12ccb5a997c3c7a7501b06097235114d83dcca
MohammadJavad Alaeddini <javad1379.a@gmail.com>
2020-04-09 18:01:31 +0430

    Add Fields Of Accounts

---
+++ src/main/java/model/Account.java
@@ -0,0 +1,18 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Account {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private String role;
+    private List<String> discountCodes;
+    private long credit;
+    private List<Log> logs;
+
+}
